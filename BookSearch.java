67e37e7797baa6f9cae90baff466f385f4b76fd0
AdrianFWM <adrianf0912@gmail.com>
2022-10-15 15:33:13 -0700

    Books

---
+++ BookSearch.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BookSearch {
+    private String BookSearch;
+
+
+    Scanner bookRead = new Scanner(System.in);
+    System.out.println("Enter your book name?");
+    bookSearch = bookRead.nextLine();
+
+
+
+}
+
+public boolean HasBook() {
+    
+}
