8105149622ba3053fcc02f2c671feae3ebd1954a
Brenner <43012551+Prelude14@users.noreply.github.com>
2022-10-17 11:47:49 -0700

    edited book class to include it's waitlist count

--- Book.java
+++ Book.java
@@ -5,18 +5,21 @@ public class Book {
     private String BookTitle;
     private String BookAuthor;
     private boolean IsAvailable;
+    private int wl; //wl count
 
-    public Book(int ID, String BookTitle, String BookAuthor) {
+    public Book(int ID, String BookTitle, String BookAuthor, int wl) {
         this.ID = ID;
         this.BookTitle = BookTitle;
         this.BookAuthor = BookAuthor;
         this.IsAvailable = true;
+        this.wl = wl;
     }
     public Book() {
         ID = 0;
         BookTitle = "";
         BookAuthor = "";
         IsAvailable = false;
+        wl = 0;
     }
     public int getID() {
         return ID;
@@ -27,6 +30,9 @@ public class Book {
     public String getBookAuthor() {
         return BookAuthor;
     }
+    public String getwl() {
+        return wl;
+    }
     public void setID(int ID) {
         this.ID = ID;
     }
@@ -36,6 +42,9 @@ public class Book {
     public void setBookAuthor(String BookAuthor) {
         this.BookAuthor = BookAuthor;
     }
+    public void setwl(int wl) {
+        this.wl = wl;
+    }
     public void setIsAvailable(boolean IsAvailable) {
         this.IsAvailable = IsAvailable;
     }
@@ -43,6 +52,6 @@ public class Book {
         return this.IsAvailable ? "Yes" : "No";
     }
     public String toString( ) {
-        return ID + " " + BookTitle + " " + BookAuthor;
+        return ID + " " + BookTitle + " " + BookAuthor + " " + wl;
     }
 }
