07e5415e8a7b128f0de66b396c7169a15c68addc
Keizo <90278067+Keizo410@users.noreply.github.com>
2022-10-17 23:53:53 -0700

    Update dbconnct.java

--- dbconnct.java
+++ dbconnct.java
@@ -1,21 +1,33 @@
 
 import java.sql.*;
+import java.util.Scanner;
 import java.io.*;
 
 public class dbconnct {
 	
 	public static void main (String[] args) throws SQLException {
-		Login l1 = new Login();
-		if(l1.HasRegistry()) {
-			System.out.println("Login Success!");
+		if(HasAccount()) {
+		   Login l1 = new Login();
+		}else{
+		   createaccount a1 = new createaccount();
+		}
+	}
+	
+	public static boolean HasAccount() {
+		String b;
+		System.out.println("Do you have library account?: y or n");
+		Scanner c = new Scanner(System.in);
+		b = c.next();
+		if(b.equals("y")) {
+			return true;
 		}else {
-			System.out.println("No account matched...");
+			System.out.println("you dont have account...Try Register!!");
+		    return false;
 		}
 	}
 	
 	public Connection dbc() throws SQLException{
-
-		String url = "jdbc:mysql://127.0.0.1:3306/CheckConnection";
+		String url = "jdbc:mysql://127.0.0.1:3306/LibMan";
 		String username = "root";
 		String password ="kkato41496746";
 		Connection con = DriverManager.getConnection(url,username,password);
