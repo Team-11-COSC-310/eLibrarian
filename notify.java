d4c64113d5d2acdf639ed96ee92cf71d2b628c73
bolu2710 <bolu2710@gmail.com>
2022-11-08 12:34:28 -0800

    done it

---
+++ notify.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
+
+public class notify extends User {
+    String receive_email = super.getEmail();
+    String send_email = "library@books.com";
+    String host = "localhost";
+
+    Properties properties = new Properties();p
+    roperties.setProperty("mail.smtp.host",host);
+    Session session = Session.getDefaultInstance(properties);
+
+    try
+    {
+        MimeMessage message = new MimeMessage(session);
+
+        message.setFrom(new InternetAddress(send_email));
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress(receive_email));
+        message.setSubject("Due Date is Close");
+        message.setText("Hi, your due date is close");
+
+        Transport.send(message);
+        System.out.println("It has Sent");
+
+    }catch(
+    MessagingException ex)
+    {
+        ex.printStackTrace();
+    }
+
+}}
