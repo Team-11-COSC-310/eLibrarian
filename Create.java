083b5a16c444c0922fc7b428c148aa3bb411570c
Keizo <90278067+Keizo410@users.noreply.github.com>
2022-11-10 11:25:44 -0800

    Update Create.java

--- Create.java
+++ Create.java
@@ -6,7 +6,7 @@ public class Create extends Login{
 	private String userPassword;
     private int cAttempt;
     
-    public Create(int cattempt) throws SQLException, ClassNotFoundException {
+    public Create(int cattempt, int user) throws SQLException, ClassNotFoundException {
 		super(cattempt,null);//call to login function with blank slate
 
 		userEmail=super.getEmail();
@@ -16,9 +16,14 @@ public class Create extends Login{
             cattempt = 3;
             setAttempt(cattempt);//store construct attempt # in variable to be seen by main app
         }
-		else { //if the email doesn't match, then make new user. DON'T care about repeat passwords
+        else if(super.HasEmail()==false&&user == 0){ //if the email doesn't match, then make new user. DON'T care about repeat passwords
             Register(userEmail,userPassword);//get login info and put it in database
             cattempt = 1; //reset create input
+            setAttempt(cattempt);//store construct attempt # in variable to be seen by main app
+        }else{
+             Register(userEmail,userPassword);
+            RegisterLibrarian(userEmail,userPassword);
+            cattempt = 2; //reset create input
             setAttempt(cattempt);//store construct attempt # in variable to be seen by main app
         }
 	}
@@ -38,4 +43,11 @@ public class Create extends Login{
         stmt.setString(2, super.PasswordEncryption(password));
         stmt.executeUpdate();
         }
+     public void RegisterLibrarian(String email, String password) throws SQLException, ClassNotFoundException{
+            String sql = "insert into librarians(email, password)" + "values (?, ?)";
+            PreparedStatement stmt = getConnect().prepareStatement(sql);
+            stmt.setString(1, email);
+            stmt.setString(2, super.PasswordEncryption(password));
+            stmt.executeUpdate();
+            }
   }
