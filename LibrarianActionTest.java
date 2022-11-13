00f43ce2746033aef9fce8056211d47e5da21d1a
Keizo <90278067+Keizo410@users.noreply.github.com>
2022-11-13 12:48:13 -0800

    Test is done with my librarianAction class.

--- LibrarianActionTest.java
+++ LibrarianActionTest.java
@@ -1,7 +1,4 @@
-import org.junit.Test;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import java.sql.SQLException;
 
@@ -9,41 +6,93 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-class LibrarianActionTest {
-	private static LibrarianAction a;
+import org.junit.Test;
 
+public class LibrarianActionTest {
+    private static LibrarianAction ac = new LibrarianAction();
+    private static Create mock = new Create();
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		System.out.println("Using @BeforeClass, executed before all test cases ");
+		System.out.println("LibrarianAction Testing-----------------------start");
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
-		System.out.println("Using @AfterClass, executed after all test cases ");
+		System.out.println("LibrarianAction Testing-----------------------end");
+
 	}
 
 	@Before
 	public void setUp() throws Exception {
-		a = new LibrarianAction();
+		
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		ac.EditBook("try");
 	}
 
 	@Test
 	public void testAddBook() throws ClassNotFoundException, SQLException {
-		assertFalse(a.AddBook());
+		assertTrue(ac.AddBook("try", "try", "try", true, 0));
 	}
 	
+
 	@Test
-	public void testDeleteUser() throws ClassNotFoundException, SQLException {
-		assertFalse(a.DeleteUser("aaa@icloud.com"));
+	public void testAddBookEmptyTitleFail() throws ClassNotFoundException, SQLException {
+		assertFalse(ac.AddBook("","try", "try", true, 0));
 	}
+	
+
 	@Test
-	public void testHasBook() throws ClassNotFoundException, SQLException {
-		assertFalse(a.HasBook("book"));
+	public void testAddBookAvailabilityFail() throws ClassNotFoundException, SQLException {
+		assertFalse(ac.AddBook("try","try", "try", null, 0));
 	}
 
+
+	@Test
+	public void testHasBook() throws ClassNotFoundException, SQLException {
+		assertFalse(ac.HasBook("try"));
+	}
+	
+
+	@Test
+	public void testHasBookEmptyTitleFail() throws ClassNotFoundException, SQLException {
+		assertFalse(ac.HasBook(""));
+	}
+	
+
+	@Test
+	public void testHasBookNonExisitingTitleFail() throws ClassNotFoundException, SQLException {
+		assertFalse(ac.HasBook("try1"));
+	}
+
+
+
+	@Test
+	public void testDeleteUser() throws ClassNotFoundException, SQLException {
+		mock.Register("mock@mock.com","mock");
+		assertTrue(ac.DeleteUser("mock@mock.com"));
+	}
+	
+
+	@Test
+	public void testDeleteUserAdmin() throws ClassNotFoundException, SQLException {
+		mock.RegisterLibrarian("mock@mock.com","mock");
+		assertTrue(ac.DeleteUser("mock@mock.com"));
+	}
+	
+
+	@Test
+	public void testDeleteUserFail() throws ClassNotFoundException, SQLException {
+		mock.Register("mock@mock.com","mock");
+		assertFalse(ac.DeleteUser("mck@mock.com"));
+		ac.DeleteUser("mock@mock.com");
+	}
+	
+	
+	
+	
+	
 
 }
